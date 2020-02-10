4661360f85f6bcf6328cceb6893edffca2d3ddd7
Khandakar Anim Hassan Adnan <55645398+adnan116@users.noreply.github.com>
2020-02-10 10:42:00 +0600

    CG_Lab-3
    Assignment Train Station

--- Source.cpp
+++ Source.cpp
@@ -1,120 +1,114 @@
 #include <windows.h>  // for MS Windows
 #include <GL/glut.h>  // GLUT, include glu.h and gl.h
+#include<math.h>>
+# define PI           3.14159265358979323846
+
 
 void display() {
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
-              glClear(GL_COLOR_BUFFER_BIT);
-              glBegin(GL_LINES);
-              glColor3f(0.2,0.60,0.50);
-              glVertex2f(-1,0);
-              glVertex2f(1,0);
-			  glVertex2f(0,-1);
-              glVertex2f(0,1);
-              glEnd();
+    glClear(GL_COLOR_BUFFER_BIT);
+              
 
-			  /*glBegin(GL_TRIANGLES);
-              glColor3f(0.5,0.90,0.8560);
-              glVertex2f(0.5,0);
-              glVertex2f(0,0.5);
-			  glVertex2f(-0.5,0);
+	glBegin(GL_QUADS);		//sky
+    glColor3ub(51, 204, 255);
+    glVertex2f(1,0.3);
+    glVertex2f(1,1);
+	glVertex2f(-1,1);
+    glVertex2f(-1,0.3);
+    glEnd();
 
-			  glColor3f(0.2,0.60,0.50);
-			  glVertex2f(0.5,0);
-			  glVertex2f(-0.5,0);
-              glVertex2f(0,-0.5);
+	GLfloat x=-.7f; GLfloat y=.75f; GLfloat radius =.15f;
+	int triangleAmount = 20; //# of triangles used to draw circle
 
-              glEnd();*/
+	//GLfloat radius = 0.8f; //radius
+	GLfloat twicePi = 2.0f * PI;
 
-			  glBegin(GL_QUADS);
-              glColor3f(0.5,0.90,0.856);
-              glVertex2f(0.7,0.75);
-              glVertex2f(0.3,0.75);
-			  glVertex2f(0.3,0.25);
-              glVertex2f(0.7,0.25);
-              glEnd();
+	glBegin(GL_TRIANGLE_FAN);			//sun
+	glColor3ub(252, 212, 64);
+		glVertex2f(x, y); // center of circle
+		for(int i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
 
-			  glBegin(GL_TRIANGLES);
-              glColor3f(0.565,0.25,0.60);
-              glVertex2f(-0.1,0.25);
-              glVertex2f(-0.4,0.5);
-			  glVertex2f(-0.7,0.25);
-			  glEnd();
+	glBegin(GL_QUADS);		//grass
+    glColor3ub(0, 153, 51);
+    glVertex2f(1,0.3);
+	glVertex2f(-1,0.3);
+    glVertex2f(-1,0);
+	glVertex2f(1,0);
+    glEnd();
 
-			  glBegin(GL_POLYGON);
-              glColor3f(0.185,0.69,0.50);
-              glVertex2f(-0.3,-0.4);
-              glVertex2f(-0.3,-0.3);
-			  glVertex2f(-0.4,-0.25);
-              glVertex2f(-0.7,-0.25);
-			  glVertex2f(-0.8,-0.3);
-			  glVertex2f(-0.8,-0.4);
-              glVertex2f(-0.7,-0.45);
-			  glVertex2f(-0.4,-0.45);
-			  glEnd();
+	glBegin(GL_QUADS);		//road
+    glColor3ub(25, 51, 77);
+    glVertex2f(1,0);
+	glVertex2f(-1,0);
+	glVertex2f(-1,-0.3);
+	glVertex2f(1,-0.3);
+    glEnd();
 
-			  glBegin(GL_LINES);
-              glColor3f(0.298,0.11,0.589);
-              glVertex2f(0.9,-0.25);
-              glVertex2f(0.3,-0.25);
+	glBegin(GL_QUADS);		//station room
+    glColor3ub(0, 102, 255);
+    glVertex2f(0.7,0);
+	glVertex2f(0.7,0.4);
+	glVertex2f(-0.2,0.4);
+	glVertex2f(-0.2,0);
+    glEnd();
 
-			  glVertex2f(0.3,-0.25);
-              glVertex2f(0.6,-0.5);
+	glBegin(GL_QUADS);		//station room roof
+    glColor3ub(102, 153, 153);
+    glVertex2f(0.9,0.4);
+	glVertex2f(0.55,0.6);
+	glVertex2f(-0.05,0.6);
+	glVertex2f(-0.4,0.4);
+    glEnd();
 
-			  glVertex2f(0.6,-0.5);
-              glVertex2f(0.9,-0.25);
-              glEnd();
+	glBegin(GL_QUADS);		//station room window1
+    glColor3ub(102, 153, 153);
+    glVertex2f(0.6,0.35);
+	glVertex2f(0.4,0.35);
+	glVertex2f(0.4,0.25);
+	glVertex2f(0.6,0.25);
+    glEnd();
 
-			  /*glBegin(GL_LINES);
-              glColor3f(0.1,1,0.25);
-              glVertex2f(0,-1);
-              glVertex2f(0,1);
-              glEnd();*/
+	 glBegin(GL_LINES);
+     glColor3ub(0,0,0);
+     glVertex2f(0.6,0.3);
+     glVertex2f(0.4,0.3);
+	 glVertex2f(0.5,.25);
+     glVertex2f(0.5,.35);
+     glEnd();
 
-			  /*glBegin(GL_QUADS);
+	glBegin(GL_QUADS);		//station room window2
+    glColor3ub(102, 153, 153);
+    glVertex2f(0.1,0.35);
+	glVertex2f(-0.1,0.35);
+	glVertex2f(-0.1,0.25);
+	glVertex2f(0.1,0.25);
+    glEnd();
 
-              glColor3ub(134, 0, 125);
-              glVertex2f(0.9,0.2);
-              glVertex2f(0.9,0.3);
-			  glVertex2f(-0.1,0.3);
-              glVertex2f(-0.1,0.2);
+	 glBegin(GL_LINES);
+     glColor3ub(0,0,0);
+     glVertex2f(0.1,0.3);
+     glVertex2f(-0.1,0.3);
+	 glVertex2f(0,.25);
+     glVertex2f(0,.35);
+     glEnd();
 
-			  glColor3ub(0, 0, 249);
-              glVertex2f(0.9,0.3);
-              glVertex2f(0.9,0.4);
-			  glVertex2f(-0.1,0.4);
-              glVertex2f(-0.1,0.3);
+	glBegin(GL_QUADS);		//station room door
+    glColor3ub(102, 153, 153);
+    glVertex2f(0.15,0);
+	glVertex2f(0.35,0);
+	glVertex2f(0.35,0.3);
+	glVertex2f(0.15,0.3);
+    glEnd();
 
-			  glColor3ub(0, 128, 24);
-              glVertex2f(0.9,0.4);
-              glVertex2f(0.9,0.5);
-			  glVertex2f(-0.1,0.5);
-              glVertex2f(-0.1,0.4);
 
-			  glColor3ub(255, 255, 65);
-              glVertex2f(0.9,0.5);
-              glVertex2f(0.9,0.6);
-			  glVertex2f(-0.1,0.6);
-              glVertex2f(-0.1,0.5);
 
-			  glColor3ub(255, 165, 44);
-              glVertex2f(0.9,0.6);
-              glVertex2f(0.9,0.7);
-			  glVertex2f(-0.1,0.7);
-              glVertex2f(-0.1,0.6);
-
-			  glColor3ub(255, 0, 24);
-              glVertex2f(0.9,0.7);
-              glVertex2f(0.9,0.8);
-			  glVertex2f(-0.1,0.8);
-              glVertex2f(-0.1,0.7);
-
-			  glColor3ub(128, 0, 0);
-              glVertex2f(-0.1,0.8);
-              glVertex2f(-0.15,0.8);
-			  glVertex2f(-0.15,-0.8);
-              glVertex2f(-0.1,-0.8);
-
-              glEnd();*/
+			 
 	glFlush();
 }
 
