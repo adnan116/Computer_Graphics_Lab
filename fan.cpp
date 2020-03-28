52bca634abc44f0d1167b0c98daebabf213a6862
Khandakar Anim Hassan Adnan <55645398+adnan116@users.noreply.github.com>
2020-03-28 21:49:04 +0600

    final fan design

--- fan.cpp
+++ fan.cpp
@@ -5,30 +5,35 @@
 # define PI           3.14159265358979323846
 
 GLfloat j = 0;
-GLfloat position = 0.0f;
-GLfloat speed = 0.0f;
 
-void update(int value) {
-    if(position >1.0)
-        position = -1.0f;
-    position += speed;
-	glutPostRedisplay();
-	glutTimerFunc(100, update, 0);
-}
+
 void Idle()
 {
     glutPostRedisplay();
 }
 
+void speedUp(int value){
+	j+=0.3;
+	glutPostRedisplay();
+	glutTimerFunc(1,speedUp,0);
+}
+
+void speedDown(int value){
+	j-=0.3;
+	glutPostRedisplay();
+	glutTimerFunc(1,speedDown,0);
+}
 
 void display() {
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
 
+
+	//fan
 	glPushMatrix();
 	glRotatef(j,0.0,0.0,1);
     glBegin(GL_QUADS);
-    glColor3ub(155,123,139);
+    glColor3ub(255,255,255);
 	glVertex2f(0,.1);
 	glVertex2f(0,-.1);
 	glVertex2f(.7,-.1);
@@ -36,7 +41,7 @@ void display() {
 	glEnd();
 
     glBegin(GL_QUADS);
-    glColor3ub(155,123,139);
+    glColor3ub(255,255,255);
 	glVertex2f(0,.1);
 	glVertex2f(0,-.1);
 	glVertex2f(-.7,-.1);
@@ -44,7 +49,7 @@ void display() {
 	glEnd();
 
 	glBegin(GL_QUADS);
-    glColor3ub(155,123,139);
+    glColor3ub(255,255,255);
 	glVertex2f(.1,0);
 	glVertex2f(.1,.7);
 	glVertex2f(-.1,.7);
@@ -52,16 +57,16 @@ void display() {
 	glEnd();
 
 	glBegin(GL_QUADS);
-    glColor3ub(155,123,139);
+    glColor3ub(255,255,255);
 	glVertex2f(.1,0);
 	glVertex2f(.1,-.7);
 	glVertex2f(-.1,-.7);
 	glVertex2f(-.1,0);
 	glEnd();
 
+    glPopMatrix();
 
 
-	//glLoadIdentity();
 
 	//fan circle
 
@@ -103,43 +108,111 @@ void display() {
 	glFlush();
 }
 
+void nonRotateDisplay() {
+	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	//fan
+    glBegin(GL_QUADS);
+    glColor3ub(255,255,255);
+	glVertex2f(0,.1);
+	glVertex2f(0,-.1);
+	glVertex2f(.7,-.1);
+	glVertex2f(.7,.1);
+	glEnd();
+
+    glBegin(GL_QUADS);
+    glColor3ub(255,255,255);
+	glVertex2f(0,.1);
+	glVertex2f(0,-.1);
+	glVertex2f(-.7,-.1);
+	glVertex2f(-.7,.1);
+	glEnd();
+
+	glBegin(GL_QUADS);
+    glColor3ub(255,255,255);
+	glVertex2f(.1,0);
+	glVertex2f(.1,.7);
+	glVertex2f(-.1,.7);
+	glVertex2f(-.1,0);
+	glEnd();
+
+	glBegin(GL_QUADS);
+    glColor3ub(255,255,255);
+	glVertex2f(.1,0);
+	glVertex2f(.1,-.7);
+	glVertex2f(-.1,-.7);
+	glVertex2f(-.1,0);
+	glEnd();
 
 
-void handleKeypress(unsigned char key, int x, int y) {
-	switch (key) {
-	case 'a':
-		j+=.1;
-		glPopMatrix();
-		break;
-	case 'b':
-	    glPopMatrix();
-		j+=.2;
-		break;
-    case 'c':
-        glPopMatrix();
-		j+=.3;
-		break;
-    case 'd':
-        glPopMatrix();
-		j+=.4;
-		break;
-    case 'e':
-        glPopMatrix();
-		j+=.5;
-		break;
-	glutPostRedisplay();
-	}
+
+	//fan circle
+
+	GLfloat x=.0f; GLfloat y=.0f; GLfloat radius =.2f;
+	int triangleAmount = 500;
+	GLfloat twicePi = 2.0f * PI;
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3ub(192,192,192);
+		glVertex2f(x, y);
+		for(int i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+
+
+    x=.0f;  y=.0f;  radius =.15f;
+    twicePi = 2.0f * PI;
+	glBegin(GL_TRIANGLE_FAN);
+	glColor3ub(255,255,255);
+		glVertex2f(x, y);
+		for(int i = 0; i <= triangleAmount;i++) {
+			glVertex2f(
+		            x + (radius * cos(i *  twicePi / triangleAmount)),
+			    y + (radius * sin(i * twicePi / triangleAmount))
+			);
+		}
+	glEnd();
+	glLoadIdentity();
+	glLineWidth(2.5);
+
+
+	glFlush();
 }
+
+
+
+void speedControl(int key, int x, int y)
+{
+	switch(key)
+		{
+		case GLUT_KEY_UP:
+		glutDisplayFunc(display);
+		glutIdleFunc(Idle);
+		glutTimerFunc(0,speedUp,0);
+		break;
+		case GLUT_KEY_DOWN:
+		glutDisplayFunc(display);
+		glutIdleFunc(Idle);
+		glutTimerFunc(0,speedDown,0);
+		break;
+		}
+	glutPostRedisplay();
+}
+
 
 
 void handleMouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON){
-        glPopMatrix();
-        j=0;
+        glutDisplayFunc(nonRotateDisplay);
 	}
 	else if (button == GLUT_RIGHT_BUTTON){
-        glPopMatrix();
-        j+=.1;
+        glutDisplayFunc(display);
+		glutIdleFunc(Idle);
+		glutTimerFunc(0,speedUp,0);
 	}
 
 	glutPostRedisplay();
@@ -149,12 +222,11 @@ void handleMouse(int button, int state, int x, int y) {
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutCreateWindow(" Analog Animated Clock");
+	glutCreateWindow("Celling Fan");
 	glutInitWindowSize(320, 320);
 	glutDisplayFunc(display);
-	glutKeyboardFunc(handleKeypress);
     glutMouseFunc(handleMouse);
-	glutIdleFunc(Idle);
+	glutSpecialFunc(speedControl);
 	glutMainLoop();
 	return 0;
 }
